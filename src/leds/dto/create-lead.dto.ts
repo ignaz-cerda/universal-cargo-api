a246a9556015a2d3017b53ced3a407ccc2bea633
skytacus <jignacio.cerda@hotmail.com>
2026-08-13 14:36:26 -0600

    Add validations

--- src/leds/dto/create-lead.dto.ts
+++ src/leds/dto/create-lead.dto.ts
@@ -1,6 +1,26 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateLeadDto {
+  @IsString({
+    message: 'El campo name debe ser un string',
+  })
+  @IsNotEmpty({
+    message: 'El campo name debe ser un string'
+  })
   name: string;
+
+  @IsEmail({
+    message: 'El email es invalido'
+  })
   email: string;
+
+  @IsString({
+    message: 'El campo name debe ser un string'
+  })
   company: string;
+
+  @IsNotEmpty({
+    message: 'El campo name debe ser un string'
+  })
   message: string;
 }
