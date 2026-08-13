b0957bf076849171a3feb4e1d7c2045e42ccc295
skytacus <jignacio.cerda@hotmail.com>
2026-08-13 15:01:54 -0600

    Add health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LeadsModule } from './leds/leads.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [LeadsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
